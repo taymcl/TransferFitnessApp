e6fcab255c31b5df7aab461be96b2409eb0ac4b3
Taylor Ann McLean <88504985+taymcl@users.noreply.github.com>
2023-05-10 22:14:05 -0400

    try to record and save distance

--- app/src/main/java/com/example/fitnesstracker/ui/activity_log/ActivityLogFragment.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/activity_log/ActivityLogFragment.kt
@@ -1,8 +1,13 @@
 package com.example.fitnesstracker.ui.activity_log
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +16,6 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -22,23 +26,23 @@ import com.example.fitnesstracker.MyRecyclerAdapter
 import com.example.fitnesstracker.R
 import com.example.fitnesstracker.databinding.FragmentActivityLogBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.FitnessActivities
 import com.google.android.gms.fitness.FitnessOptions
-import com.google.android.gms.fitness.data.DataType
-import com.google.android.gms.fitness.data.Session
+import com.google.android.gms.fitness.data.*
+import com.google.android.gms.fitness.request.DataReadRequest
+import com.google.android.gms.fitness.request.SessionInsertRequest
+import com.google.android.gms.fitness.request.SessionReadRequest
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
-enum class FitActionRequestCode {
-    SUBSCRIBE,
-    CANCEL_SUBSCRIPTION,
-    DUMP_SUBSCRIPTIONS
-}
+import java.util.concurrent.TimeUnit
 
 
 class ActivityLogFragment : Fragment() {
     private lateinit var thiscontext: Context
 
     private var _binding: FragmentActivityLogBinding? = null
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -46,14 +50,16 @@ class ActivityLogFragment : Fragment() {
     private var inSession: Boolean = false
     private var startTime: Long = 0
     private var endTime: Long = 0
-    private val button = view?.findViewById<Button>(R.id.session_button)
+    private var sessionName = ""
 
 
-    private val fitnessOption = FitnessOptions.builder()
+    private val fitnessOptions = FitnessOptions.builder()
         .addDataType(DataType.TYPE_DISTANCE_DELTA, FitnessOptions.ACCESS_WRITE)
-        .addDataType(DataType.TYPE_ACTIVITY_SEGMENT, FitnessOptions.ACCESS_WRITE)
+        .addDataType(DataType.TYPE_DISTANCE_DELTA, FitnessOptions.ACCESS_READ)
         .build()
 
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -64,6 +70,14 @@ class ActivityLogFragment : Fragment() {
             ViewModelProvider(this)[ActivityLogViewModel::class.java]
 
         _binding = FragmentActivityLogBinding.inflate(inflater, container, false)
+
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getAccountForExtension(requireContext(), fitnessOptions), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                requireActivity(), // your activity
+                1, // e.g. 1
+                GoogleSignIn.getAccountForExtension(requireContext(), fitnessOptions),
+                fitnessOptions)
+        }
 
         val sessionButton: Button = binding.sessionButton
         sessionButton.setOnClickListener {
@@ -81,88 +95,162 @@ class ActivityLogFragment : Fragment() {
     }
 
     private fun startSession() {
-        // Check if user is authenticated for Google Play Services
-        val account = GoogleSignIn.getAccountForExtension(requireContext(), fitnessOption)
-        if (!GoogleSignIn.hasPermissions(account, fitnessOption)) {
-            GoogleSignIn.requestPermissions(
-                this,
-                1,
-                account,
-                fitnessOption
-            )
-            return
-        }
-
         inSession = true
         Toast.makeText(context, "Activity session started!", Toast.LENGTH_SHORT).show()
 
-        // Create new session and start recording data
+        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US)
+        sessionName = "Activity Session ${sdf.format(Date())}"
 
         // Start recording the time
         startTime = System.currentTimeMillis()
+
+        Fitness.getRecordingClient(requireActivity(), GoogleSignIn.getAccountForExtension(requireContext(), fitnessOptions))
+            // This example shows subscribing to a DataType, across all possible data
+            // sources. Alternatively, a specific DataSource can be used.
+            .subscribe(DataType.TYPE_DISTANCE_DELTA)
+
     }
 
 
+    @SuppressLint("SimpleDateFormat")
     private fun endSession() {
+
         inSession = false
         Toast.makeText(context, "Activity session ended!", Toast.LENGTH_SHORT).show()
         val dbHelper = ActivityDbHelper(thiscontext)
 
         // Calculate the time
+
+        var distance: Double = 0.0
         endTime = System.currentTimeMillis()
-        val tDelta: Long = endTime - startTime
-        var elapsedSeconds: Double = (tDelta / 1000.0) / 60
-        val formattedMinutes = String.format("%.2f", elapsedSeconds)
 
-        // Get the date
-        val calendar: Calendar = Calendar.getInstance()
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
-        val currentDate: String = dateFormat.format(calendar.getTime())
 
-        // Add session info to the database
-        dbHelper.insertData("Dist", currentDate, formattedMinutes)
-        //createFromDb()
+        val distanceDataSource = DataSource.Builder()
+            .setAppPackageName(requireContext().packageName)
+            .setDataType(DataType.TYPE_DISTANCE_DELTA)
+            .setStreamName("$sessionName-distance")
+            .setType(DataSource.TYPE_RAW)
+            .build()
+
+        // Build a dataset that includes the session data
+        val distanceDp = DataPoint.builder(distanceDataSource)
+            .setField(Field.FIELD_DISTANCE, distance.toFloat())
+            .setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS)
+            .build()
+
+        val dataset = DataSet.builder(distanceDataSource)
+            .add(distanceDp)
+            .build()
+
+        val newSession = Session.Builder()
+            .setName(sessionName)
+            .setIdentifier("UniqueIdentifierHere")
+            .setDescription("Walk")
+            .setActivity(FitnessActivities.WALKING)
+            .setStartTime(startTime, TimeUnit.MILLISECONDS)
+            .setEndTime(endTime, TimeUnit.MILLISECONDS)
+            .build()
+
+
+        val insertrequest = SessionInsertRequest.Builder()
+            .setSession(newSession)
+            .addDataSet(dataset)
+            .build()
+
+
+        // Invoke the SessionsClient with the session identifier
+        Fitness.getSessionsClient(requireActivity(), GoogleSignIn.getAccountForExtension(requireContext(), fitnessOptions))
+            .insertSession(insertrequest)
+            .addOnSuccessListener {
+                Log.i(TAG, "Session inserted")
+                Fitness.getRecordingClient(
+                    requireActivity(),
+                    GoogleSignIn.getAccountForExtension(requireContext(), fitnessOptions)
+                )
+                    .unsubscribe(DataType.TYPE_DISTANCE_DELTA)
+            }
+
+        val readRequest = SessionReadRequest.Builder()
+            .read(DataType.TYPE_DISTANCE_DELTA)
+            .setSessionId(newSession.identifier)
+            .setTimeInterval(newSession.getStartTime(TimeUnit.MILLISECONDS), newSession.getEndTime(TimeUnit.MILLISECONDS), TimeUnit.MILLISECONDS)
+            .build()
+
+
+        Fitness.getSessionsClient(requireActivity(), GoogleSignIn.getAccountForExtension(requireContext(), fitnessOptions))
+            .readSession(readRequest)
+            .addOnSuccessListener { response ->
+                val sessions = response.sessions
+                Log.i(TAG, "Number of returned sessions is: ${sessions.size}")
+                for (session in sessions) {
+                    val dataSets = response.getDataSet(session)
+                    for (dataSet in dataSets) {
+                        for (dataPoint in dataSet.dataPoints) {
+                            distance = dataPoint.getValue(Field.FIELD_DISTANCE).asFloat().toDouble()
+                            Log.i(TAG, "Distance: $distance")
+                        }
+                    }
+                }
+                val tDelta: Long = endTime - startTime
+                val elapsedSeconds: Double = (tDelta / 1000.0) / 60
+                val formattedMinutes = String.format("%.2f", elapsedSeconds)
+
+                // Get the date
+                val calendar: Calendar = Calendar.getInstance()
+                val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+                val currentDate: String = dateFormat.format(calendar.getTime())
+
+                // Add session info to the database
+                dbHelper.insertData(distance.toString(), currentDate, formattedMinutes)
+                //createFromDb()
+            }
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView: RecyclerView = view.findViewById<View>(com.example.fitnesstracker.R.id.recycler_view) as RecyclerView
+        val recyclerView: RecyclerView =
+            view.findViewById<View>(R.id.recycler_view) as RecyclerView
 
         // Build the recyclerView from the database
         recyclerView.adapter = MyRecyclerAdapter(createFromDb())
 
         recyclerView.layoutManager = LinearLayoutManager(activity)
         // Divider
-        val dividerItemDecoration = DividerItemDecoration(activity, DividerItemDecoration.VERTICAL)
+        val dividerItemDecoration =
+            DividerItemDecoration(activity, DividerItemDecoration.VERTICAL)
         recyclerView.addItemDecoration(dividerItemDecoration)
 
         // Check if location and activity recognition permissions are granted
 
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != android.content.pm.PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
                 requireContext(),
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != android.content.pm.PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
                 requireContext(),
-                android.Manifest.permission.ACTIVITY_RECOGNITION
-            ) != android.content.pm.PackageManager.PERMISSION_GRANTED
+                Manifest.permission.ACTIVITY_RECOGNITION
+            ) != PackageManager.PERMISSION_GRANTED
         ) {
-           return
+            return
         }
 
     }
 
-    private fun createFromDb() : ArrayList<LoggedActivity>{
+    private fun createFromDb(): ArrayList<LoggedActivity> {
         val dbHelper = ActivityDbHelper(thiscontext)
         val activities = ArrayList<LoggedActivity>()
         val cursor = dbHelper.viewAllData
 
-        while (cursor.moveToNext()){
-            val activity = LoggedActivity("Date: ${cursor.getString(2)}", " Time Elapsed: ${cursor.getString(3)}", "Distance: ${cursor.getString(1)}")
+        while (cursor.moveToNext()) {
+            val activity = LoggedActivity(
+                "Date: ${cursor.getString(2)}",
+                " Time Elapsed: ${cursor.getString(3)}",
+                "Distance: ${cursor.getString(1)}"
+            )
             activities.add(activity)
         }
         return activities
@@ -172,5 +260,5 @@ class ActivityLogFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
 }
+
